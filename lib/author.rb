be91477bc8dd3594e52be54a8bf1351dfa17bde4
MJ Johnson <mjohnson4685@gmail.com>
2020-09-21 20:59:33 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -14,9 +14,9 @@ class Author
   def add_post(post)
     post.author = self
   end
-  def add_song_by_name(name)
-    song = Post.new(name)
-    song.artist = self
+  def add_post_by_title(name)
+    post = Post.new(name)
+    post.author = self
   end
   def self.post_count
     Post.all.count
