6db1c99c12bb9a167e54c0a92bc328b66b54b48b
MJ Johnson <mjohnson4685@gmail.com>
2020-09-21 20:51:26 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,14 +1,14 @@
 class Post
-  attr_accessor :name, :author
+  attr_accessor :title, :author
   @@all = []
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
     @@all << self
   end
   def self.all
     @@all
   end
-  def author_name
-    author.name if author
+  def author_title
+    author.title if author
   end
 end
