b1310571bfd11cd4eaac3344755da6ced0b79d20
MJ Johnson <mjohnson4685@gmail.com>
2020-09-21 20:52:58 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,15 +1,15 @@
 class Author
-  attr_accessor :name
+  attr_accessor :title
   @@all = []
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
     @@all << self
   end
   def self.all
     @@all
   end
-  def songs
-    Song.all.select {|song| song.artist == self}
+  def posts
+    Post.all.select {|post| post.artist == self}
   end
   def add_song(song)
     song.artist = self
@@ -19,6 +19,6 @@ class Author
     song.artist = self
   end
   def self.song_count
-    Song.all.count
+    Post.all.count
   end
 end
