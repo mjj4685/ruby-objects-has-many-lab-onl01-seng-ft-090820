fa89fdc4e456374e2c04ba9f94eef3a999de6ced
MJ Johnson <mjohnson4685@gmail.com>
2020-09-21 20:45:40 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,38 +1,24 @@
 class Artist
-
   attr_accessor :name
-
   @@all = []
-
   def initialize(name)
     @name = name
     @@all << self
   end
-
-  def songs
-    song.all.select {|song| song.artist == self}
+  def self.all
+    @@all
   end
-
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
   def add_song(song)
     song.artist = self
   end
-
   def add_song_by_name(name)
     song = Song.new(name)
     song.artist = self
-
   end
-
   def self.song_count
-    song.all.count
+    Song.all.count
   end
-
-  def self.all
-    @all
-  end
-
-
-
 end
-
-adele = Artist.new("Adele")
